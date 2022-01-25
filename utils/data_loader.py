84993b047d8daba0f38f5049cd1c01baa6551d30
Yang Xiao <swagshaw1998@gmail.com>
2022-01-25 23:51:17 +0800

    :sparkles: feat: speech_data_loader

--- utils/data_loader.py
+++ utils/data_loader.py
@@ -12,9 +12,63 @@ from typing import List
 import numpy as np
 import pandas as pd
 import torch
+import torchaudio
 from torch.utils.data import Dataset
+import torch.nn.functional as F
 
 logger = logging.getLogger()
+
+
+class SpeechDataset(Dataset):
+    def __init__(self, data_frame: pd.DataFrame, dataset: str, transform=None):
+        super(SpeechDataset, self).__init__()
+        """
+        Args:
+            data_frame: pd.DataFrame
+            filename: train_filename or valid_filename
+            is_training: True or False
+        """
+        self.data_frame = data_frame
+        self.dataset = dataset
+        self.transform = transform
+        self.sampling_rate = 16000
+        self.sample_length = 16000
+
+    def __len__(self):
+        return len(self.data_frame)
+
+    def load_audio(self, speech_path):
+        waveform, sr = torchaudio.load(speech_path)
+        if waveform.shape[1] < self.sample_length:
+            # padding if the audio length is smaller than samping length.
+            waveform = F.pad(waveform, [0, self.sample_length - waveform.shape[1]])
+
+        if self.is_training == True:
+            pad_length = int(waveform.shape[1] * 0.1)
+            waveform = F.pad(waveform, [pad_length, pad_length])
+            offset = torch.randint(0, waveform.shape[1] - self.sample_length + 1, size=(1,)).item()
+            waveform = waveform.narrow(1, offset, self.sample_length)
+        return waveform
+
+    def __getitem__(self, idx):
+        sample = dict()
+        if torch.is_tensor(idx):
+            idx = idx.tolist()
+
+        file_name = self.data_frame.iloc[idx]["file_name"]
+        label = self.data_frame.iloc[idx].get("label", -1)
+
+        audio_path = os.path.join("dataset/data", self.dataset, file_name)
+        waveform = self.load_audio(audio_path)
+        if self.transform:
+            waveform = self.transform(waveform)
+        sample["waveform"] = waveform
+        sample["label"] = label
+        sample["file_name"] = file_name
+        return sample
+
+    def get_audio_class(self, y):
+        return self.data_frame[self.data_frame["label"] == y]
 
 
 def get_train_datalist(args, cur_iter: int) -> List:
