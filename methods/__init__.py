fc4b2500848cf86240eb34ab45704743d2db494c
Yang Xiao <swagshaw1998@gmail.com>
2022-01-25 23:22:08 +0800

    :tada: initialize the structure

---
+++ methods/__init__.py
@@ -0,0 +1,7 @@
+"""
+# !/usr/bin/env python
+-*- coding: utf-8 -*-
+@Time    : 2022/1/25 下午11:05
+@Author  : Yang "Jan" Xiao 
+@Description : __init__.py
+"""
---
+++ utils/__init__.py
@@ -0,0 +1,7 @@
+"""
+# !/usr/bin/env python
+-*- coding: utf-8 -*-
+@Time    : 2022/1/25 下午10:39
+@Author  : Yang "Jan" Xiao 
+@Description : 
+"""
